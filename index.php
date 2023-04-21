6ddb463f81e206e2842779098338bddb4ef6a2eb
Roger Junior <roger@rogerjunior.com>
2023-04-21 11:55:46 +0100

    Update 1.1

--- index.php
+++ index.php
@@ -12,3 +12,38 @@ function add_admin_theme()
 }
 
 add_action('admin_head', 'add_admin_theme');
+
+//
+
+function remove_dashboard_meta()
+{
+    remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
+    remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
+    remove_meta_box('dashboard_primary', 'dashboard', 'side');
+    remove_meta_box('dashboard_secondary', 'dashboard', 'normal');
+    remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
+    remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+    remove_meta_box('dashboard_right_now', 'dashboard', 'normal');
+    remove_meta_box('dashboard_activity', 'dashboard', 'normal'); //since 3.8
+    remove_meta_box('hmwp_dashboard_widget', 'dashboard', 'normal');
+    remove_meta_box('dashboard_site_health', 'dashboard', 'normal');
+    remove_meta_box('aioseo-rss-feed', 'dashboard', 'normal');
+    remove_meta_box('aioseo-overview', 'dashboard', 'normal');
+}
+
+add_action('admin_init', 'remove_dashboard_meta');
+
+function rgrjnr_add_dashboard_widgets()
+{
+    global $wp_meta_boxes;
+
+    wp_add_dashboard_widget('rgrjnr_help_widget', 'Bem-vindo', 'rgrjnr_dashboard_help');
+}
+
+function rgrjnr_dashboard_help()
+{
+    echo '<p>Olá, se você tiver alguma dúvida, problema ou solicitação, pode entrar em contato diretamente com <a href="mailto:roger@rogerjunior.com">roger@rogerjunior.com</a>.</p>';
+}
+
+add_action('wp_dashboard_setup', 'rgrjnr_add_dashboard_widgets');
