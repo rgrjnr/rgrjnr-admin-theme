d3717510ca7f91fa920385df89db7488413b9bb0
Roger Junior <roger@rogerjunior.com>
2023-04-21 11:40:55 +0100

    Update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once dirname(__FILE__) . '/update.php';
 // Style admin
 function add_admin_theme()
 {
-    $css_uri = RGRJNR_ADMIN_ASSETS_URL . '/css/stylesheet.css?ver=';
+    $css_uri = RGRJNR_ADMIN_ASSETS_URL . '/css/stylesheet.css?ver=1';
     echo '<link rel="stylesheet" href="' . $css_uri . '" type="text/css" media="all" />';
 }
 
